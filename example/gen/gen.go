eed6a82fef28ba59ae369e13d5e974911b89d719
Mads Ovesen <m.ovesen@allsaints.com>
2019-08-24 00:01:13 +0100

    test

--- example/gen/gen.go
+++ example/gen/gen.go
@@ -4,7 +4,7 @@ import (
 	"encoding/xml"
 	"time"
 
-	"github.com/hooklift/gowsdl/soap"
+	"github.com/madshov/gowsdl/soap"
 )
 
 // against "unused imports"
--- go.mod
+++ go.mod
@@ -1,3 +1,1 @@
 module github.com/madshov/gowsdl
-
-require github.com/hooklift/gowsdl v0.3.1
