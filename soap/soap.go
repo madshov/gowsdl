4b88d399f4e679a6d626754087d8decbc46ead1d
Mads Ovesen <m.ovesen@allsaints.com>
2019-08-27 20:57:03 +0100

    debug

--- soap/soap.go
+++ soap/soap.go
@@ -357,7 +357,7 @@ fmt.Printf("\nhere ---->%+v\n", req)
 	} else {
 		dec = xml.NewDecoder(res.Body)
 	}
-fmt.Printf("\nhere 2---->%+vi %+v\n", res.Bod, mtomBoundaryy)
+fmt.Printf("\nhere 2---->%+vi %+v\n", res.Body, mtomBoundary)
 	if err := dec.Decode(respEnvelope); err != nil {
 		return err
 	}
